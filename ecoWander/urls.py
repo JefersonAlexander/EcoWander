7a3bfd18d16ee8cd96ecb2c1967a5bebecc40779
Jeferson Alexander Del Rio Herrera <yeferdrh@gmail.com>
2024-10-10 23:03:38 -0500

    Creating base template and navigation bar

--- ecoWander/urls.py
+++ ecoWander/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('ecoWanderApp.urls')),
     path('authentication/', include('authentication.urls')),
 ]
---
+++ ecoWanderApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EcowanderappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ecoWanderApp'
